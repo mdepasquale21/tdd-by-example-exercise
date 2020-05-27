76ef7a84da59edc41cd12d7522ab2fc206d2da77
mdepasquale <depasquale@esteco.com>
2020-05-27 17:03:28 +0200

    second cycle finished, now dollar returns a new dollar object when performing multiplication

--- spec/test-multiplication.spec.ts
+++ spec/test-multiplication.spec.ts
@@ -1,11 +1,21 @@
 import { Dollar } from '../src/dollar';
 
 describe('', () => {
+    let five: Dollar;
 
-    it('five dollars multiplied by two is ten dollars', () => {
-        const five = new Dollar(5);
-        five.times(2)
-        expect(five.amount).toEqual(10)
+    beforeEach(() => {
+        five = new Dollar(5);
+    });
+
+    it('5 dollars times 2 is 10 dollars', () => {
+        const product: Dollar = five.times(2)
+        expect(product.amount).toEqual(10)
+
+    });
+
+    it('5 dollars times 3 is 15 dollars', () => {
+        const product: Dollar = five.times(3)
+        expect(product.amount).toEqual(15)
     });
 
 });
--- src/dollar.ts
+++ src/dollar.ts
@@ -2,8 +2,8 @@ export class Dollar {
     constructor(public amount: number) {
     }
 
-    public times(multiplier: number): void {
-        this.amount *= multiplier;
+    public times(multiplier: number): Dollar {
+        return new Dollar(this.amount * multiplier)
     }
 
 }
