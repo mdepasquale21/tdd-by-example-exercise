6ed6338ba41e07f5565a594903e1a979a5eb4ce8
mdepasquale <depasquale@esteco.com>
2020-06-03 23:23:43 +0200

    refactor test a bit

---
+++ spec/test-reduce-money.spec.ts
@@ -0,0 +1,66 @@
+import { Bank } from '../src/bank';
+import { Money } from '../src/money/money';
+
+describe('Bank reduce', () => {
+
+    it('Bank should reduce money', () => {
+        const bank: Bank = new Bank();
+        const result: Money = bank.reduce(Money.dollar(1), 'USD');
+        expect(result).toEqual(Money.dollar(1));
+    });
+
+});
+
+describe('Reduce same Currency', () => {
+
+    it('1 dollar should be 1 dollar', () => {
+        const bank: Bank = new Bank();
+        const dollar: Money = Money.dollar(1);
+
+        const result: Money = bank.reduce(dollar, 'USD');
+
+        expect(result).toEqual(Money.dollar(1));
+    });
+
+    it('3 dollars should be 3 dollars', () => {
+        const bank: Bank = new Bank();
+        const dollar: Money = Money.dollar(3);
+
+        const result: Money = bank.reduce(dollar, 'USD');
+
+        expect(result).toEqual(Money.dollar(3));
+    });
+
+    it('conversion rate should be 1 from one currency to the same currency', () => {
+        const bank: Bank = new Bank();
+
+        expect(bank.rate('USD', 'USD')).toEqual(1);
+    });
+
+});
+
+describe('Reduce different Currencies', () => {
+
+    it('2 francs should be 1 dollar ', () => {
+        const bank: Bank = new Bank();
+        const francs2: Money = Money.franc(2);
+
+        bank.addRate('CHF', 'USD', 2);
+
+        const result: Money = bank.reduce(francs2, 'USD');
+
+        expect(result).toEqual(Money.dollar(1));
+    });
+
+    it('4 francs should be 2 dollars ', () => {
+        const bank: Bank = new Bank();
+        const francs4: Money = Money.franc(4);
+
+        bank.addRate('CHF', 'USD', 2);
+
+        const result: Money = bank.reduce(francs4, 'USD');
+
+        expect(result).toEqual(Money.dollar(2));
+    });
+
+});
